41beb069ba1b12a6e20778373d27622d3ed80c68
Hampiz <hampus.bengtsson@elev.ga.ntig.se>
2025-09-11 16:40:03 +0200

    first lesson

--- app.rb
+++ app.rb
@@ -1,1 +1,23 @@
 require 'sinatra'
+require 'slim'
+require 'sinatra/reloader'
+
+get ('/home') do
+    slim(:home)
+end
+
+get ('/about') do
+    slim(:about)
+end
+
+get ('/test') do
+    @fruitdata = "hamps"
+    slim(:test)
+end
+
+get ('/fruits/:id') do
+    fruits = ["banan", "melon", "kiwi", "citron"]
+    id = params[:id].to_i
+    @fruit = fruits[id]
+    slim(:fruits)
+end
